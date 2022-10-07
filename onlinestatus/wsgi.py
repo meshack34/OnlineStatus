94aaf70a701fa54a63811c84ae3953c4d1f8a1a6
meshack34 <meshackkimutai34@gail.com>
2022-10-07 08:09:16 +0300

    onlinestatus

---
+++ onlinestatus/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onlinestatus project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinestatus.settings')
+
+application = get_wsgi_application()
